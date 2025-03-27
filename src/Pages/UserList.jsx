16868e3900a7e662e26d21838a3562c7bdf70953
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-03-27 20:32:37 +0530

    showing user list

--- src/Pages/UserList.jsx
+++ src/Pages/UserList.jsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect } from "react";
 import axios from "axios";
 import { UserContext } from "../contextCreate/Userdata";
+import Paging from "../components/Paging";
 
 function UserList() {
-  const { user, setUser, loading } = useContext(UserContext);
-  const { allUser, page } = user;
+  const { user, setUser } = useContext(UserContext);
+  const { allUser, page, loading } = user;
 
   const baseURl = "https://reqres.in";
 
@@ -18,7 +19,7 @@ function UserList() {
       setUser((prev) => ({ ...prev, allUser: res.data.data, loading: false }));
     } catch (error) {
       console.error("Error fetching users:", error);
-    //   setUser((prev) => ({ ...prev, loading: false }));
+      //   setUser((prev) => ({ ...prev, loading: false }));
     }
   };
 
@@ -31,25 +32,31 @@ function UserList() {
   }
 
   return (
-    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {allUser.map((user) => (
-        <div
-          key={user.id}
-          className="bg-white p-4 rounded shadow-md flex items-center"
-        >
-          <img
-            src={user.avatar}
-            alt={user.first_name}
-            className="w-16 h-16 rounded-full mr-4"
-          />
-          <div>
-            <h3 className="text-lg font-semibold">
-              {user.first_name} {user.last_name}
-            </h3>
-            <p className="text-gray-600">{user.email}</p>
+    <div>
+      <div className="p-4 flex flex-wrap justify-center gap-4">
+        {allUser.map((user) => (
+          <div
+            key={user.id}
+            className="bg-white p-4 rounded-lg shadow-md flex items-center w-full xs:w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 transition-all duration-300 hover:shadow-lg"
+          >
+            <img
+              src={user.avatar}
+              alt={user.first_name}
+              className="w-16 h-16 rounded-full mr-4 object-cover"
+            />
+            <div>
+              <h3 className="text-lg font-semibold text-gray-800">
+                {user.first_name} {user.last_name}
+              </h3>
+              <p className="text-gray-600 text-sm">{user.email}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-4">
+        <Paging />
+      </div>
     </div>
   );
 }
