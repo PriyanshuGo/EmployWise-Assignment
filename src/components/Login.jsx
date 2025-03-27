46530b142012653a4411e2acb75c0b363ddc4141
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-03-27 23:53:47 +0530

    error handing with login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,20 +5,24 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("eve.holt@reqres.in");
   const [password, setPassword] = useState("cityslicka");
+  const [error, setError] = useState(""); 
+
   const navigate = useNavigate();
 
   const baseURl = "https://reqres.in"
   const handleLogin = async (e) => {
     e.preventDefault();
-    console.log("🔑 Attempting login with email:", email);
+    setError(""); 
+    console.log("🔑 Attempting login ");
 
     try {
         const res = await axios.post(`${baseURl}/api/login`,{email,password})
         console.log("✅ Login successful! Token:");
         localStorage.setItem("token",res.data.token);
-        navigate("/user")
+        navigate("/userList")
     } catch (error) {
-      alert("Invalid email or password. Please try again.");
+      setError("Invalid email or password. Please try again.");
+      console.log("Error during login:", error);
     }
   }
   
@@ -26,6 +30,7 @@ function Login() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white p-6 rounded shadow-md w-full max-w-sm">
         <h2 className="text-xl font-semibold text-center mb-4">Login</h2>
+        {error && <p className="text-red-500 text-center">{error}</p>} {/* Show error message */}
         <form onSubmit={handleLogin}>
           <input
             type="email"
@@ -45,7 +50,7 @@ function Login() {
           />
           <button
             type="submit"
-            className="w-full bg-blue-500  py-2 rounded"
+            className="w-full bg-blue-500 py-2 rounded cursor-pointer"
           >
             Login
           </button>
